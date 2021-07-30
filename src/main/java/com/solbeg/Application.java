00d129013e41a617bf44533a73189c796e9191d1
dmitry.sayushev <dmitry_sayushev@solbeg.soft>
2021-07-30 17:33:28 +0300

    secure swagger

--- src/main/java/com/solbeg/Application.java
+++ src/main/java/com/solbeg/Application.java
@@ -2,9 +2,12 @@ package com.solbeg;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -15,6 +18,8 @@ import io.swagger.v3.oas.annotations.info.License;
                 contact = @Contact(url = "https://example.com", name = "support", email = "support@example.com")
         )
 )
+@SecurityRequirement(name = "Demo authorization")
+@SecurityScheme(name = "Demo authorization", type = SecuritySchemeType.HTTP, scheme = "Bearer", bearerFormat = "JWT")
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
